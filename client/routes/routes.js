4f1dc2ee9390647b8525f79b203afeeb2b934c77
erasaur <erasaur@gmail.com>
2014-11-04 18:39:07 -0800

    Upgrade to Meteor v1.0, fix routes for iron router v1.0

--- client/routes/routes.js
+++ client/routes/routes.js
@@ -8,38 +8,35 @@ Router.plugin('dataNotFound', { dataNotFoundTemplate: 'notFound' });
 
 Router.onBeforeAction(function () {
   if (!Meteor.loggingIn() && !Meteor.user())
-    this.redirect("home");
+    this.render('home');
 
-}, {except: ["home"]});
+}, {except: ['home']});
 
 Router.onBeforeAction(function () {
   if (Meteor.user()) {
-    if (!!Session.get("currentRoom"))
-      this.redirect("game");
+    if (!!Session.get('currentRoom'))
+      this.render('game');
     else 
-      this.redirect("rooms");
+      this.render('rooms');
   }
 
-}, {only: ["home"]});
+}, {only: ['home']});
 
-Router.map(function() {
-  this.route("home", { 
-    path: "/"
-  });
-  this.route("rooms", {
-    path: "/rooms"
-    // , waitOn: function () {
-    //   return Meteor.subscribe("rooms");
-    // }
-  });
-  this.route("game", {
-    path: "/rooms/:_id",
-    // , waitOn: function () {
-    //   return Meteor.subscribe("room", this.params._id); // players (filtered by online), entities, etc
-    // },
-    data: function () {
-      Session.set("currentRoom", this.params._id);
-      //return Rooms.findOne(this.params._id);
-    }
-  });
+Router.route('/', {
+  name: 'home'
+});
+Router.route('/rooms', {
+  // waitOn: function () {
+  //   return Meteor.subscribe('rooms');
+  // }
+});
+Router.route('/rooms/:_id', {
+  name: 'game',
+  // waitOn: function () {
+  //   return Meteor.subscribe('room', this.params._id); // players (filtered by online), entities, etc
+  // },
+  data: function () {
+    Session.set('currentRoom', this.params._id);
+    //return Rooms.findOne(this.params._id);
+  }
 });
