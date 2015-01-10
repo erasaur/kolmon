392fa3bd6c72409959b2ba2d15eb2eae05196fe0
erasaur <erasaur@gmail.com>
2015-01-09 23:11:45 -0800

    User field for challenges

--- collections/users.js
+++ collections/users.js
@@ -21,6 +21,9 @@ Schema.UserGame = new SimpleSchema({
   },
   direction: { // 0 - static, 1 - up, 2 - left, 3 - right, 4 - bottom
     type: Number
+  },
+  challenges: {
+    type: Object
   }
 });
 
