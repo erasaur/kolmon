9f5b5a6ab38f02124c40375254159b5dd543852c
erasaur <erasaur@gmail.com>
2015-01-18 21:54:49 -0800

    Publish challenges for current user

--- server/publications/current_user.js
+++ server/publications/current_user.js
@@ -1,7 +1,13 @@
 Meteor.publish('currentUser', function () {
   if (!this.userId) return;
 
-  return Meteor.users.find(this.userId, { 
-    fields: { 'profile': 1, 'stats': 1, 'game': 1 } 
-  });
+  return [
+    Meteor.users.find(this.userId, { 
+      fields: { 'profile': 1, 'stats': 1, 'game': 1 } 
+    }),
+    Challenges.find({ $or: [
+      { 'sent.id': this.userId },
+      { 'received.id': this.userId }
+    ]})
+  ];
 });
