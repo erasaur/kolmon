034b146951c9e1ffa940a531db1aa9624a3ca3e0
erasaur <erasaur@gmail.com>
2015-01-19 19:02:11 -0800

    Fix TTL index

--- collections/challenges.js
+++ collections/challenges.js
@@ -9,6 +9,11 @@ ChallengeSchema = new SimpleSchema({
   roomId: {
     type: String
   },
+  status: {
+    type: Number,
+    allowedValues: [STATUS_PENDING, STATUS_ACCEPTED, STATUS_REJECTED],
+    defaultValue: STATUS_PENDING
+  },
   sender: {
     type: Object
   },
@@ -20,6 +25,7 @@ ChallengeSchema = new SimpleSchema({
 Challenges = new Mongo.Collection('challenges');
 // Challenges.attachSchema(ChallengeSchema);
 
+// XXX move to server-side?
 Meteor.methods({
   challengeSend: function (player) {
     var user = Meteor.user();
@@ -28,6 +34,7 @@ Meteor.methods({
     var challenge = {
       createdAt: new Date(),
       roomId: user.game.roomId,
+      status: STATUS_PENDING,
       sender: {
         id: user._id,
         username: user.username
@@ -41,7 +48,28 @@ Meteor.methods({
     // check(challenge, ChallengeSchema);
     Challenges.insert(challenge);
   },
-  challengeAccept: function (player) {
-    // TODO
+  challengeAccept: function (challenge) {
+    if (!this.userId) return;
+    // check(challenge, Match.ObjectIncluding({
+    //   sender: Object,
+    //   receiver: Object
+    // }));
+
+    var sender = Meteor.users.findOne(challenge.sender.id);
+    var receiver = Meteor.users.findOne(challenge.receiver.id);
+
+    if (sender.inBattle || receiver.inBattle)
+      throw new Meteor.Error('already-battling', 'One of the users on either side of the challenge is already in battle');
+
+    Challenges.update(challenge._id, { $set: { status: STATUS_ACCEPTED } });
+    Meteor.users.update({ _id: { $in: [sender,receiver] } }, {
+      $set: { 'game.inBattle': true }
+    }, { multi: true });
+  },
+  challengeReject: function (challenge) {
+    if (!this.userId) return;
+    Challenges.update(challenge._id, {
+      $set: { status: STATUS_REJECTED }
+    });
   }
 });
