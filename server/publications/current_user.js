3e390e6443f40b6d7f07a01af645a42a85f9f636
erasaur <erasaur@gmail.com>
2015-05-09 13:07:30 -0700

    Major refactor: pull collections from KOL, add wip pubs for pokes

--- server/publications/current_user.js
+++ server/publications/current_user.js
@@ -1,7 +1,29 @@
+var Users = KOL.Users;
+
 Meteor.publish('currentUser', function () {
   if (!this.userId) return this.ready();
 
-  return Meteor.users.find(this.userId, { 
-    fields: { 'profile': 1, 'stats': 1, 'game': 1 } 
+  return Users.find(this.userId, {
+    fields: { 'profile': 1, 'stats': 1, 'playerId': 1 }
   });
 });
+
+// Meteor.publish('pokemonList', function () {
+//   if (!this.userId) return this.ready();
+
+//   return Pokemon.find({ userId: this.userId }, {
+//     fields: { 'name': 1 }
+//   });
+// });
+
+// Meteor.publish('singlePokemon', function (pokemonId) {
+//   check(pokemonId, String);
+
+//   if (!this.userId) return this.ready();
+
+//   var pokemon = Pokemon.findOne(pokemonId, { fields: { 'userId': 1 } });
+//   if (pokemon.userId !== this.userId)
+//     return this.ready();
+
+//   return Pokemon.find(pokemonId);
+// });
