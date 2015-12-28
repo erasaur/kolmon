35bc6d5498526778aa1dcee9130a60859210e48d
Gabriel Lee <lee.j.gabriel@gmail.com>
2015-12-28 15:52:13 -0800

    Add script to generate .gif links for backside of pokemon and add links-back.json

---
+++ generatelinks-back.py
@@ -0,0 +1,28 @@
+import os
+import re
+
+with open('links-back.json', 'a') as f:
+  f.write('{\n')
+  count = 0
+  for dirname, dirnames, filenames in os.walk('.'):
+
+    # print path to all subdirectories first.
+    # for subdirname in dirnames:
+    #   print(os.path.join(dirname, subdirname))
+
+    first = True
+
+    for filename in filenames:
+      match = re.search(r'[^123456789].gif', filename)
+      
+      if filename.lower().endswith('.gif') and match:
+        if first: 
+          first = False
+          f.write('  "' + str(count) + '": "' + os.path.join('http://www.pkparaiso.com/imagenes/xy/sprites/animados-espalda/', filename) + '"')
+          count += 1
+        else:
+          f.write(',\n  "' + str(count) + '": "' + os.path.join('http://www.pkparaiso.com/imagenes/xy/sprites/animados-espalda/', filename) + '"')
+          count += 1
+      
+
+  f.write('\n}')
