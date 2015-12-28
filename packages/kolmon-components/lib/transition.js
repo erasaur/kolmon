28ba434e8b5c8dc62cdd2179bf3493c40dc2c2ce
erasaur <erasaur@gmail.com>
2015-12-27 20:08:16 -0800

    Update game to handle fetching maps dynamically, and handle transitions; update transitions to render fade-in or fade-out animations

--- packages/kolmon-components/lib/transition.js
+++ packages/kolmon-components/lib/transition.js
@@ -1,3 +1,5 @@
+var constants = KOL.constants;
+
 KOL.Transition = (function () {
   function Transition (options) {
     if (!this instanceof Transition) {
@@ -10,32 +12,39 @@ KOL.Transition = (function () {
   Transition.prototype.load = function loadTransition (options) {
     var self = this;
 
+    self._type = constants.TRANSITION_FADE_OUT;
     self._renderers = options.renderers;
     self._running = false;
+    self._numFrames = constants.TRANSITION_NUM_FRAMES;
+  };
+
+  Transition.prototype.running = function getRunning () {
+    return this._running;
   };
 
   Transition.prototype.run = function runTransition (options) {
     var self = this;
-    var frames = constants.TRANSITIONS[options.state];
 
-    if (self._running || !_.isArray(frames)) {
-      options.onfinish();
+    if (self._running) {
+      self.onfinish();
     }
 
-    self._renderers.fg.loadImages({
-      srcs: frames,
-      onload: function (images) {
-        self._images = images;
-        self._current = 0;
-        self._numFrames = frames.length;
-        self._onfinish = options.onfinish;
-        self._running = true;
-      }
-    });
+    console.log('running transition');
+
+    self._type = options.type || self._type;
+    self._running = true;
+    self._current = 0;
+
+    if (_.isFunction(options.onfinish)) {
+      self._onfinish = options.onfinish;
+    }
   };
 
   Transition.prototype.onfinish = function finishTransition () {
+    var self = this;
     self._running = false;
+
+    console.log('finishing transition');
 
     if (self._onfinish) {
       self._onfinish();
@@ -44,8 +53,21 @@ KOL.Transition = (function () {
   };
 
   Transition.prototype.update = function () {
+    console.log('update');
     var self = this;
-    renderers.fg.render(self._images[self._current]);
+    var alpha = self._current / self._numFrames;
+
+    if (self._type === constants.TRANSITION_FADE_OUT) {
+      alpha = 1 - alpha;
+    }
+
+    self._renderers.transition.clear();
+    self._renderers.transition.renderRect({
+      globalAlpha: alpha,
+      x: 0, y: 0,
+      width: constants.CANVAS_WIDTH,
+      height: constants.CANVAS_HEIGHT
+    });
 
     if (++self._current >= self._numFrames) {
       self.onfinish();
